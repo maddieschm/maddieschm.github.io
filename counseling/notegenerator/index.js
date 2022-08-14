7fe5e4908877f7b7f8e7bf88e9960eb0afd2eee3
maddieschm <108240588+maddieschm@users.noreply.github.com>
2022-08-13 20:25:04 -0700

    update js logic

--- counseling/notegenerator/index.js
+++ counseling/notegenerator/index.js
@@ -3,11 +3,23 @@ function handleData()
     var location = "";
     var theory = "";
     var summary = "";
+    var diagnosis = "";
     var form_data = new FormData(document.querySelector("form"));
     console.log("form data: ");
     for(var pair of form_data.entries()) 
     {
         console.log(pair[0]+ ' : '+ pair[1]);
+        var id = pair[0];
+        switch(id) {
+            case 'location':
+                location = pair[1];
+            case 'theory':
+                theory = pair[1];
+            case 'diagnosis':
+                diagnosis = pair[1];
+            case 'summary':
+                summary = pair[1];
+        }
     }
     var sentence1 = "The clinician met with the client " + location + " to treat their diagnosis of " + diagnosis + ".";
     var sentence2 = "The clinician utilized " + theory + " to support the client in building their skills.";
